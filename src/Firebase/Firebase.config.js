fb456f02c625945329afe94bc8ac89ba1b8a53d2
HelloAyan <18103373@iubat.edu>
2023-11-10 12:31:50 +0600

    firebase config, Routes, Main layout added

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB9Sqn1hIQTZFCRyTYF-OjuH9a9q8dsuKg",
+    authDomain: "travel-agency-5b06c.firebaseapp.com",
+    projectId: "travel-agency-5b06c",
+    storageBucket: "travel-agency-5b06c.appspot.com",
+    messagingSenderId: "973355309374",
+    appId: "1:973355309374:web:9e8fd3a8864b3e8f91346f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
